9397a32dc3e87d50900eeae037cfff2d15928d66
dhanush2bot <149936819+dhanush2bot@users.noreply.github.com>
2023-12-20 14:08:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/NobiDeveloper/Nobita-Filter-Bot.git /Nobita-Filter-Bot
+  git clone https://github.com/dhanush2bot/nobiss.git /nobiss
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Nobita-Filter-Bot
+  git clone $UPSTREAM_REPO /nobiss
 fi
-cd /Nobita-Filter-Bot
+cd /nobiss
 pip3 install -U -r requirements.txt
-echo "Starting...."
+echo "Starting.... fuck you man"
 python3 bot.py
